d94ee846a90b579600e6ab4c448b3e50d6bcf69b
Ekita Pattjoshi <ekitapatthoshi@gmail.com>
2026-05-24 22:44:17 +0530

    updated some styles

--- src/component/navbar/navbar.js
+++ src/component/navbar/navbar.js
@@ -1,47 +1,4 @@
-// import "./navbar.css";
-
-// const Navbar = () => {
-//     return (
-//         <nav>
-//             <h2>Vacation EXP</h2>
-//         </nav>
-//     );
-// };
-
-// export default Navbar;
-
-
-// import "./navbar.css";
-
-// const Navbar = () => {
-//     return (
-//         <nav className="navbar">
-//             <div className="navbar__container">
-
-//                 <div className="navbar__logo">
-//                     <h2 className="navbar__title">Vacation EXP</h2>
-//                 </div>
-
-//                 <div className="navbar__menu">
-//                     <ul className="navbar__list">
-//                         <li className="navbar__item">Home</li>
-//                         <li className="navbar__item">Packages</li>
-//                         <li className="navbar__item">About</li>
-//                         <li className="navbar__item">Contact</li>
-//                     </ul>
-//                 </div>
-
-//                 <div className="navbar__actions">
-//                     <button className="navbar__button">
-//                         Login
-//                     </button>
-//                 </div>
-
-//             </div>
-//         </nav>
-//     );
-// };
-
+import "./navbar.css";
 import { FaPlaneDeparture } from "react-icons/fa";
 
 const Navbar = () => {
@@ -64,7 +21,7 @@ const Navbar = () => {
                 </div>
 
                 <div className="navbar__actions">
-                    <button className="navbar__button navbar__button--secondary">
+                    <button className="navbar__button navbar__button_secondary">
                         Sign Up
                     </button>
 
@@ -77,4 +34,5 @@ const Navbar = () => {
         </nav>
     );
 };
+
 export default Navbar;
